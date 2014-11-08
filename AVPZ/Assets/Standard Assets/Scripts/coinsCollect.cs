10f4d5efc4507342db732ca716eb87983489a0ec
smile437 <smile437@gmail.com>
2014-11-08 13:00:21 +0200

    coins

---
+++ AVPZ/Assets/Standard Assets/Scripts/coinsCollect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class coinsCollect : MonoBehaviour
+{
+	int score=0;
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "coin")
+		{
+			Destroy(other.gameObject);
+			score+=1;
+		}
+	}
+	void OnGUI() 
+	{
+		GUILayout.Label( "Score = " + score );
+		
+	}
+}
