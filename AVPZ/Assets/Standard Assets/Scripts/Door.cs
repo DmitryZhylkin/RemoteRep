8ae8e9eb8c724f1a03280eae32b7ac3234b40bc6
smile437 <smile437@gmail.com>
2014-11-24 17:01:13 +0200

    24,11

---
+++ AVPZ/Assets/Standard Assets/Scripts/Door.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Door : MonoBehaviour {
+
+	 
+	// Use this for initialization
+	void Start () {	
+	}
+
+	void Update () {	
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Player"){
+//GetComponent(Camera).enabled = false;
+		//	other.am.enabled = true;
+			Animator am = gameObject.GetComponent<Animator>();
+		am.enabled = true;
+		//GetComponent(animation, true);
+		}
+	}
+}
